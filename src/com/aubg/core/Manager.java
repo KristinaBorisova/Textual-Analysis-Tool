c7392b9f83e850f4cdd791df12abc60c84628086
Kristina Borisova <kkristinaborisova@gmail.com>
2021-10-08 09:31:40 +0300

    Create initial project structure

---
+++ src/com/aubg/core/Manager.java
@@ -0,0 +1,16 @@
+package com.aubg.core;
+
+public class Manager {
+
+    public void startProcess() {
+    //request user input/text file
+
+    // call features calcularions on the given input
+
+    // form linguistic signatures
+
+    // calculate similarity between features
+
+        // print final result
+    }
+}
