4bc63fdd6647279ea72202318b8b3f3cebc937bd
KristinaBorisova <kkristinaborisova@gmail.com>
2021-10-27 14:01:44 +0300

    Refactor existing code and split methods for file choosing

--- src/com/aubg/authorshipDetectionProgram/ui/ChooseFileWindow.java
+++ src/com/aubg/authorshipDetectionProgram/ui/ChooseFileWindow.java
@@ -13,24 +13,30 @@ public class ChooseFileWindow extends JComponent implements Accessible {
     }
 
     public String[] getFileContent() {
+        //create a string to hold File location
+        String fileLocation = getFileLocationFromUi();
         //create a variable to hold the file content
         String[] userFileContent = null;
         //get file content
         //..
-        // get file location
+        return userFileContent;
+    }
+
+    private String getFileLocationFromUi() {
         String fileLocation = null;
         JFileChooser fileChooser = new JFileChooser ();
-        fileChooser.addChoosableFileFilter (new FileNameExtensionFilter ("Text Files",
-                "txt",
-                "text"));
+        fileChooser.addChoosableFileFilter (new FileNameExtensionFilter ("Text Files", "txt", "text"));
         if (fileChooser.showOpenDialog (null) == JFileChooser.APPROVE_OPTION) {
             File file = fileChooser.getSelectedFile ();
             fileLocation = file.getAbsolutePath ();
-            //print file content for check
-            //pass file content to the next function
         }
-        System.out.println(fileLocation);
-        return userFileContent;
+        return fileLocation;
+    }
+
+    public String[] getContentFromFile(String fileLocation) throws IOException {
+        String[] fileContent = null;
+        //..
+        return fileContent;
     }
 
 }
