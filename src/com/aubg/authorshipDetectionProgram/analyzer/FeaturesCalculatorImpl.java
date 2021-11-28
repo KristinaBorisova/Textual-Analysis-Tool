45ae7f7c0043f1c06de3a4b235f3f27845db9680
kristinaborisova <kkristinaborisova@gmail.com>
2021-11-28 16:28:56 +0200

    Create a new class to implement the new interface method
    This commit aims to introduce a new class which will implement the "getAllFeaturesValues" method from the interface and will return the final linguistic signature with all of its values combined

---
+++ src/com/aubg/authorshipDetectionProgram/analyzer/FeaturesCalculatorImpl.java
@@ -0,0 +1,12 @@
+package com.aubg.authorshipDetectionProgram.analyzer;
+
+import com.aubg.authorshipDetectionProgram.core.FeaturesCalculator;
+
+import java.io.IOException;
+
+public class FeaturesCalculatorImpl implements FeaturesCalculator {
+    @Override
+    public LinguisticSignature getAllFeaturesValues(String text) throws IOException {
+        return null;
+    }
+}
